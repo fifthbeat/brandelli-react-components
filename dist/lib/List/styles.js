e8981902cda73d45b8e2412043502b32d29c78b1
megio <mattia.magi91@gmail.com>
2018-11-08 16:09:23 +0100

    update

--- dist/lib/List/styles.js
+++ dist/lib/List/styles.js
@@ -5,14 +5,17 @@ var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cook
 };
 exports.__esModule = true;
 var styled_components_1 = require("styled-components");
-exports.List = styled_components_1["default"].div(templateObject_2 || (templateObject_2 = __makeTemplateObject(["\n  height: 100%;\n  box-sizing: border-box;\n\n  & > * {\n    box-sizing: border-box;\n  }\n\n  & > ul {\n    display: flex;\n    flex-direction: ", ";\n    width: 100%;\n    height: ", ";\n    padding: 0;\n    border-radius: 4px;\n    overflow-y: scroll;\n\n    &::-webkit-scrollbar {\n      display: none;\n    }\n\n    ", ";\n\n    list-style-type: none;\n    & > li {\n      display: flex;\n      align-items: center;\n      border-bottom: 1px solid lightgrey;\n      padding: 0 16px;\n      line-height: 1.2;\n\n      &:last-of-type {\n        border-bottom: none;\n      }\n\n      justify-content: ", ";\n\n      min-height: ", ";\n    }\n  }\n"], ["\n  height: 100%;\n  box-sizing: border-box;\n\n  & > * {\n    box-sizing: border-box;\n  }\n\n  & > ul {\n    display: flex;\n    flex-direction: ",
+exports.List = styled_components_1["default"].div(templateObject_3 || (templateObject_3 = __makeTemplateObject(["\n  height: 100%;\n  box-sizing: border-box;\n  ", ";\n\n  & > * {\n    box-sizing: border-box;\n  }\n\n  & > ul {\n    display: flex;\n    flex-direction: ", ";\n    width: 100%;\n    height: ", ";\n    padding: 0;\n    border-radius: 4px;\n    overflow-y: scroll;\n\n    &::-webkit-scrollbar {\n      display: none;\n    }\n\n    ", ";\n\n    list-style-type: none;\n    & > li {\n      display: flex;\n      align-items: center;\n      border-bottom: 1px solid lightgrey;\n      padding: 0 16px;\n      line-height: 1.2;\n\n      &:last-of-type {\n        border-bottom: none;\n      }\n\n      justify-content: ", ";\n\n      min-height: ", ";\n    }\n  }\n"], ["\n  height: 100%;\n  box-sizing: border-box;\n  ",
+    ";\n\n  & > * {\n    box-sizing: border-box;\n  }\n\n  & > ul {\n    display: flex;\n    flex-direction: ",
     ";\n    width: 100%;\n    height: ", ";\n    padding: 0;\n    border-radius: 4px;\n    overflow-y: scroll;\n\n    &::-webkit-scrollbar {\n      display: none;\n    }\n\n    ",
     ";\n\n    list-style-type: none;\n    & > li {\n      display: flex;\n      align-items: center;\n      border-bottom: 1px solid lightgrey;\n      padding: 0 16px;\n      line-height: 1.2;\n\n      &:last-of-type {\n        border-bottom: none;\n      }\n\n      justify-content: ",
     ";\n\n      min-height: ",
     ";\n    }\n  }\n"])), function (props) {
+    return props.absolute && styled_components_1.css(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n      position: absolute;\n      top: 0;\n      left: 0;\n      right: 0;\n      bottom: 0;\n    "], ["\n      position: absolute;\n      top: 0;\n      left: 0;\n      right: 0;\n      bottom: 0;\n    "])));
+}, function (props) {
     return props.direction ? props.direction : "column";
 }, function (props) { return props.listHeight; }, function (props) {
-    return props.border && styled_components_1.css(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n        border: 1px solid lightgrey;\n      "], ["\n        border: 1px solid lightgrey;\n      "])));
+    return props.border && styled_components_1.css(templateObject_2 || (templateObject_2 = __makeTemplateObject(["\n        border: 1px solid lightgrey;\n      "], ["\n        border: 1px solid lightgrey;\n      "])));
 }, function (props) {
     switch (props.contentPosition) {
         case "end":
@@ -32,5 +35,5 @@ exports.List = styled_components_1["default"].div(templateObject_2 || (templateO
             return "40px";
     }
 });
-exports.Item = styled_components_1["default"].li(templateObject_3 || (templateObject_3 = __makeTemplateObject(["\n  list-style-type: none;\n  display: flex;\n  flex-direction: row;\n  width: 100%;\n  align-items: center;\n  box-sizing: border-box;\n  border-bottom: 1px solid lightgrey;\n  :last-of-type {\n    border: none;\n  }\n"], ["\n  list-style-type: none;\n  display: flex;\n  flex-direction: row;\n  width: 100%;\n  align-items: center;\n  box-sizing: border-box;\n  border-bottom: 1px solid lightgrey;\n  :last-of-type {\n    border: none;\n  }\n"])));
-var templateObject_1, templateObject_2, templateObject_3;
+exports.Item = styled_components_1["default"].li(templateObject_4 || (templateObject_4 = __makeTemplateObject(["\n  list-style-type: none;\n  display: flex;\n  flex-direction: row;\n  width: 100%;\n  align-items: center;\n  box-sizing: border-box;\n  border-bottom: 1px solid lightgrey;\n  :last-of-type {\n    border: none;\n  }\n"], ["\n  list-style-type: none;\n  display: flex;\n  flex-direction: row;\n  width: 100%;\n  align-items: center;\n  box-sizing: border-box;\n  border-bottom: 1px solid lightgrey;\n  :last-of-type {\n    border: none;\n  }\n"])));
+var templateObject_1, templateObject_2, templateObject_3, templateObject_4;
--- dist/lib/Switcher/index.d.ts
+++ dist/lib/Switcher/index.d.ts
@@ -6,14 +6,23 @@ interface Props {
     footer?: string;
     /** Append some text at the top */
     header?: string;
+    /** Save index of active element */
+    action?: any;
+    /** Default active element */
+    elementActive?: number;
 }
 interface State {
     /** Define the active scope */
     radio: boolean[];
 }
 export default class extends React.Component<Props, State> {
+    static getDerivedStateFromProps(props: any, state: any): {
+        radio: any;
+    };
     readonly state: State;
+    componentDidMount(): void;
     render(): JSX.Element;
+    private createRadio;
     private selectItem;
     private renderTimespanElm;
 }
